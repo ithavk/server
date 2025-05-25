d0374ee12e3b168284d9c8a342fc3c9c0df2377a
ithavk <krish.victory1@gmail.com>
2025-05-25 15:50:58 +0530

    server

--- utils/index.js
+++ utils/index.js
@@ -7,9 +7,10 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "none", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    domain: process.env.NODE_ENV === "production" ? ".hostingersite.com" : undefined, // Set domain for production
   });
 };
 
